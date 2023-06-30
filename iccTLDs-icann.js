d455f62598816ef00c73486945ba5a3b18007e84
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2023-06-30 14:43:52 +0400

    add proposed and remove example from the set.

--- iccTLDs-icann.js
+++ iccTLDs-icann.js
@@ -3,6 +3,17 @@
 
 const fs = require('fs');
 
+const examples = new Set([
+  'xn--11b5bs1di'
+]);
+
+// https://en.wikipedia.org/wiki/Proposed_top-level_domain#Internationalized_country_code_top-level_domains
+const more = new Set([
+  'xn--wgv71a',
+  'xn--vcst06ab2a',
+  'xn--mgbb7fyab'
+]);
+
 (async () => {
   const url = 'https://www.icann.org/resources/pages/string-evaluation-completion-2014-02-19-en';
   const icanres = await fetch(url);
@@ -12,9 +23,15 @@ const fs = require('fs');
 
   const icanhtml = await icanres.text();
   const punymatch = icanhtml.matchAll(/xn--[a-zA-Z0-9]+/g);
-  const matches = Array.from(punymatch).map(n => n[0].toLowerCase()).sort();
+  const matches = Array.from(punymatch);
 
-  fs.writeFileSync('./data/iccTLDs.txt', matches.join('\n') + '\n');
+  const final = matches
+      .map(n => n[0].toLowerCase())
+      .filter(n => !examples.has(n))
+      .concat(Array.from(more))
+      .sort();
+
+  fs.writeFileSync('./data/iccTLDs.txt', final.join('\n') + '\n');
 })().catch((err) => {
   console.error(err);
   process.exit(1);
